67b6d4bd0201b7700c8c477175ef0cc68b4ed7fa
HyprArch <kirito0160218@gmail.com>
2026-05-30 20:59:35 -0600

    new_hashCrack.py v2

--- new_hashCrack.py
+++ new_hashCrack.py
@@ -26,7 +26,7 @@ class hash_crack:
         argu.add_argument('-pf', '--passwords_file', required=True, dest="passwords_file", help="Write the file of the passwords or passwords separed by , | EX: -pf passwords.txt || -pf has1,has2,has3")
         argu.add_argument('-wl', '--wordlist', required=True, dest="path_wordlist", help="Choose the wordlist you will use")
         argu.add_argument('-us', '--user_in_file', dest="users_file", help="If you want, you Can provide the users for each password (separated by commas or as a file), The md5 password must match with the user, write the password and the user in the same position | EX: -pf has1,has2 -us user_has1,user_has2")
-        argu.add_argument('-m', '--mode', required=True, dest="mode_hash", help="Write the type of the passwords [-m MD5 || -m LM]")
+        argu.add_argument('-m', '--mode', required=True, dest="mode_hash", help="Write the type of the passwords [-m MD5 || -m LM || SHA1]")
 
         return argu.parse_args()
 
@@ -50,7 +50,7 @@ class hash_crack:
 
     def crack_md5_hash(self):
 
-        print(colored(f"\n{'-'*20} Cracked MD5 Hashes{'-'*20}\n", "yellow"))
+        print(colored(f"{'-'*20} Cracked MD5 Hashes{'-'*20}\n", "yellow"))
 
         bar = log.progress("Brute Forcing MD5 Hashes")
         bar.status("[+] Starting \n")
@@ -72,9 +72,9 @@ class hash_crack:
 
     def crack_lm_hash(self):
 
-        print(colored(f"\n{'-'*20} Cracked LM Hashes{'-'*20}\n", "yellow"))
+        print(colored(f"{'-'*20} Cracked LM Hashes{'-'*20}\n", "yellow"))
 
-        bar = log.progress("Brute Forcing MD5 Hashes")
+        bar = log.progress("Brute Forcing LM Hashes")
         bar.status("[+] Starting \n")
         for index,password in enumerate(self.hashed_passwords):
             for crack_pass in self.wordlist:
@@ -92,6 +92,27 @@ class hash_crack:
 
         bar.success("Brute forcing Finished")
 
+    def crack_sha1_hash(self):
+
+        print(colored(f"{'-'*20} Cracked SHA-1 Hashes{'-'*20}\n", "yellow"))
+
+        bar = log.progress("Brute Forcing SHA-1 Hashes")
+        bar.status("[+] Starting \n")
+        for index,password in enumerate(self.hashed_passwords):
+            for crack_pass in self.wordlist:
+                try:
+                    username = self.usernames[index] if self.usernames[index] else 'No_User_Provided'
+                except IndexError:
+                    username = 'No_User_Provide'
+                bar.status(f"[-] SHA1 HASH: {password} - Username: {username} - Testing: {crack_pass}")
+                try:
+                    if(password == hashlib.sha1(crack_pass.encode()).hexdigest()):
+                        log.success(f"SHA1 HASH: {password} - Username {username} - Password: {crack_pass}")
+                        break
+                except UnicodeEncodeError:
+                    continue
+
+        bar.success("Brute forcing Finished")
 
     def print_hashes_users(self):
 
@@ -118,13 +139,13 @@ class hash_crack:
             self.crack_md5_hash()
         elif (self.arguments.mode_hash.upper() == "LM"):
             self.crack_lm_hash()
+        elif (self.arguments.mode_hash.upper() == "SHA1"):
+            self.crack_sha1_hash()
         else:
-            print(colored("[!] Bad Mode: Available modes = MD5 || LM", 'red'))
+            print(colored("[!] Bad Mode: Available modes = MD5 || LM || SHA1", 'red'))
 
     def print_pannel(self):
 
-        command = 'figlet "HASH CRACK" | lolcat -f | boxes -d tex-box'
-        subprocess.run(command, shell=True)
         if self.arguments.users_file:
             print(colored(f"\n\t[+] User's File: {self.arguments.users_file}", "blue"))
         print(colored(f"\t[+] Password's File: {self.arguments.passwords_file}", "yellow"))
@@ -132,8 +153,10 @@ class hash_crack:
         print(colored(f"\t[+] Hash Mode: {self.arguments.mode_hash}\n", "cyan"))
 
 def main():
-
     
+    command = 'figlet "HASH CRACK" | lolcat -f | boxes -d tex-box'
+    subprocess.run(command, shell=True)
+
     poo_cracked = hash_crack()
     poo_cracked.print_pannel()
     poo_cracked.check_data()
